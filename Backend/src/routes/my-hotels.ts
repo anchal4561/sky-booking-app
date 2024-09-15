7b736363308b04fe326256fa610da21fe6d887ee
anchal <anchalagrwal37@gmail.com>
2024-09-15 16:55:51 +0530

    ediy my hotels

--- Backend/src/routes/my-hotels.ts
+++ Backend/src/routes/my-hotels.ts
@@ -39,13 +39,7 @@ router.post("/",
 
     
     //1. upload images to cloudinary
-    const uploadPromises= imageFiles.map(async(image)=>{
-        const b64= Buffer.from(image.buffer).toString("base64")//encoding in base64
-        let dataURI= "data:"+image.mimetype+ ";base64,"+ b64;//craete a string that describe image encoded as base64 image
-        const res=await cloudinary.v2.uploader.upload(dataURI);// cloudinary sdk to upload images
-        return res.url;
-    })
-    const imageUrls= await Promise.all(uploadPromises);// resolevs url of uploaded images
+    const imageUrls = await uploadImages(imageFiles);// resolevs url of uploaded images
 // wait for all images upload promises to resolve and collects the resulting urls into 'imageurls' arrat
     //2. if upload was successfull , add the urls to new hotel
     newHotel.imageUrls=imageUrls;
@@ -74,5 +68,58 @@ router.get("/",verifyToken,async(req:Request,res:Response)=>{
         res.status(500).json({message:"Error fetching hotels"})
     }
 })
+
+
+// hotel by id->
+router.get("/:id",verifyToken,async(req: Request,res:Response)=>{
+    const id= req.params.id.toString();
+    try {
+        const hotel= await Hotel.findOne({
+            _id: id,
+            userId: req.userId
+        })
+        res.json(hotel);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching hotels" });
+    }
+})
+
+//update
+router.put("/:hotelId",verifyToken,upload.array("imageFiles"),async(req:Request,res:Response)=>{
+    try {
+        const updatedHotel:HotelType=req.body;
+        updatedHotel.lastUpdated=new Date();
+        const hotel=await Hotel.findOneAndUpdate({
+            _id:req.params.hotelId,
+            userId:req.userId
+        },updatedHotel,{new:true})
+        
+        if(!hotel){
+            return res.status(404).json({message:"Hotel not found"});
+
+        }
+
+        const files=req.files as Express.Multer.File[];
+        const updatedIamgeUrls=await uploadImages(files);
+        hotel.imageUrls=[...updatedIamgeUrls,
+            ...(updatedHotel.imageUrls||[])
+        ];
+        await hotel.save();
+        res.status(201).json(hotel);
+    } catch (error) {
+        res.status(500).json({message:"Someting went wrong"})
+    }
+})
+
+async function uploadImages(imageFiles: Express.Multer.File[]) {
+    const uploadPromises = imageFiles.map(async (image) => {
+        const b64 = Buffer.from(image.buffer).toString("base64"); //encoding in base64
+        let dataURI = "data:" + image.mimetype + ";base64," + b64; //craete a string that describe image encoded as base64 image
+        const res = await cloudinary.v2.uploader.upload(dataURI); // cloudinary sdk to upload images
+        return res.url;
+    });
+    const imageUrls = await Promise.all(uploadPromises); // resolevs url of uploaded images
+    return imageUrls;
+}
 
 export default router;
--- Frontend/src/forms/MangaeHotelForm/ManageHotelForm.tsx
+++ Frontend/src/forms/MangaeHotelForm/ManageHotelForm.tsx
@@ -4,6 +4,8 @@ import TypeSection from "./TypeSection";
 import FacilitiesSection from "./FacilitiesSection";
 import GuestSection from "./GuestSection";
 import ImageSection from "./ImageSection";
+import { HotelType } from "../../../../Backend/src/shared/types";
+import { useEffect } from "react";
 
 export type HotelFormData = {
   name: string;
@@ -14,22 +16,30 @@ export type HotelFormData = {
   pricePerNight: number;
   starRating: number;
   facilities: string[];
+  imageUrls:string[],
   imageFiles: FileList;
   adultCount: number;
   childCount: number;
 }
 
 type Props={
+  hotel?:HotelType;
   onSave:(hotelFormData: FormData)=> void
   isLoading:boolean
 }
 
-const ManageHotelForm = ({onSave, isLoading}:Props) => {
+const ManageHotelForm = ({onSave, isLoading,hotel}:Props) => {
   const formMethods = useForm<HotelFormData>();
-  const {handleSubmit}= formMethods;
+  const {handleSubmit,reset}= formMethods;
+  useEffect(()=>{
+    reset(hotel);
+  },[hotel,reset])
   const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
     console.log(formDataJson)
     const formData = new FormData();
+    if(hotel){
+      formData.append("hotelId",hotel._id)
+    }
     formData.append("name", formDataJson.name);
     formData.append("city", formDataJson.city);
     formData.append("country", formDataJson.country);
@@ -44,6 +54,13 @@ const ManageHotelForm = ({onSave, isLoading}:Props) => {
       formData.append(`facilities[${index}]`, facility);
     });
 
+    //[image1.jpg,image2.jpg]
+     // imageUrls=[image1.jpg]
+    if(formDataJson.imageUrls){
+      formDataJson.imageUrls.forEach((url,index)=>{
+        formData.append(`imageUrls[${index}]`,url)
+      })
+    }
 
     Array.from(formDataJson.imageFiles).forEach((imageFile) => {
       formData.append(`imageFiles`, imageFile);
